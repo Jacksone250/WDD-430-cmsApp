077755c9264fb521e2eec5bd1c2f732238f5cd26
Jacksone250 <Jacksone250@gmail.com>
2020-09-25 09:36:15 -0600

    Contact list finished

--- src/app/contacts/contacts-detail/contacts-detail.component.ts
+++ src/app/contacts/contacts-detail/contacts-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contacts-detail',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts-detail.component.css']
 })
 export class ContactsDetailComponent implements OnInit {
+  contact: Contact = new Contact('3', 'Tim Thayne', 'thaynet@byui.edu', '208-496-3777 ', 'https://web.byui.edu/Directory/Employee/thaynet.jpg', null);
 
   constructor() { }
 
--- src/app/contacts/contacts-list/contacts-list.component.ts
+++ src/app/contacts/contacts-list/contacts-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contacts-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts-list.component.css']
 })
 export class ContactsListComponent implements OnInit {
+  contacts: Contact[] = [
+    new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', 'https://web.byui.edu/Directory/Employee/jacksonk.jpg', null),
+    new Contact('2', 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', 'https://web.byui.edu/Directory/Employee/barzeer.jpg', null),
+    new Contact('3', 'Tim Thayne', 'thaynet@byui.edu', '208-496-3777 ', 'https://web.byui.edu/Directory/Employee/thaynet.jpg', null)
+  ];
 
   constructor() { }
 
